314b19663b7cad34053509a7ab90f6cd9c241b30
Wim Zwart <wwzwart@gmail.com>
2020-02-20 13:00:07 +0000

    UPdate on Synch scripts

--- Cluster/script_utils/rxsync.sh
+++ Cluster/script_utils/rxsync.sh
@@ -4,7 +4,7 @@
 echo "You are $USER"
 if [ "$USER" == "wwzwart" ]; then
 echo "RX Sync in Progress"
-echo K-58%U6m | sudo -S rsync -azv  --exclude 'data'  --exclude '.git' --exclude '__pycache__' --exclude '.idea' --exclude '\#015' -e"sshpass -p "w*wZ47Ws" ssh -o StrictHostKeyChecking=no -A s1999534@student.ssh.inf.ed.ac.uk ssh" s1999534@mlp1:mlpractical/ /mnt/c/Data\ Science/Machine\ Learning\ Practical,\ MLP/MLP_Project/Server/
+echo K-58%U6m | sudo -S rsync -azv  --exclude 'data'  --exclude 'exp_basic/save_models/'  --exclude '.git' --exclude '__pycache__' --exclude '.idea' --exclude '\#015' -e"sshpass -p "w*wZ47Ws" ssh -o StrictHostKeyChecking=no -A s1999534@student.ssh.inf.ed.ac.uk ssh" s1999534@mlp1:mlpractical/ /mnt/c/Data\ Science/Machine\ Learning\ Practical,\ MLP/MLP_Project/Cluster/
 echo "RX Sync Finished"
 fi
 
