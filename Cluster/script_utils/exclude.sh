39e23a29472d9fe899b7f728453f92c550f489ac
Wim Zwart <wwzwart@gmail.com>
2020-02-21 13:10:38 +0000

    Improved Sync Scripts

---
+++ Cluster/script_utils/exclude.sh
@@ -0,0 +1,2 @@
+echo "Setting user credentials"
+exclude=(--exclude 'data' --exclude 'exp_basic/saved_models/'  --exclude '.git' --exclude '__pycache__' --exclude '.idea' --exclude '\#015')
--- Cluster/script_utils/paths.sh
+++
@@ -1,5 +0,0 @@
-echo "hi"
-local_path="/mnt/c/Data Science/Machine Learning Practical, MLP/MLP_Project/Cluster/"
-exclude="--exclude 'data'  --exclude 'exp_basic/saved_models/'  --exclude '.git' --exclude '__pycache__' --exclude '.idea' --exclude '\#015'"
-exclude="--exclude 'data'"
-ARGS=(--exclude 'data')
