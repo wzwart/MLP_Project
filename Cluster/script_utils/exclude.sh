8cb31a7dfe02b7275b6caf5686971780d3cc027a
Wim Zwart <wwzwart@gmail.com>
2020-02-24 21:53:40 +0000

    Merged with Youtube HM

--- Cluster/script_utils/exclude.sh
+++ Cluster/script_utils/exclude.sh
@@ -1,2 +1,3 @@
 echo "Setting user credentials"
-exclude=(--exclude 'data' --exclude 'exp_basic/saved_models/' --exclude 'exp_unet/saved_models/' --exclude 'notes/'  --exclude 'script_utils/'   --exclude '.git' --exclude '__pycache__' --exclude '.idea' --exclude '\#015')
+tx_exclude=(--exclude 'data' --exclude 'exp_basic/saved_models/' --exclude 'exp_unet/saved_models/' --exclude 'notes/'  --exclude 'script_utils/'   --exclude '.git' --exclude '__pycache__' --exclude '.idea' --exclude '\#015')
+rx_exclude=(--exclude 'data' --exclude '*.py' --exclude '*.json' --exclude '*.sh' --exclude 'exp_basic/saved_models/' --exclude 'exp_unet/saved_models/' --exclude 'notes/'  --exclude 'script_utils/'   --exclude '.git' --exclude '__pycache__' --exclude '.idea' --exclude '\#015')
