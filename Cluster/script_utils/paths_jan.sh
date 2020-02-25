a60a4ac9b861b423cd3773e3d39e6eef1012b5e3
jango2296 <57230375+jango2296@users.noreply.github.com>
2020-02-25 12:40:38 +0000

    change jan datatxsync

--- Cluster/script_utils/paths_jan.sh
+++ Cluster/script_utils/paths_jan.sh
@@ -1,5 +1,5 @@
 echo "Setting The Paths"
 local_path="/Users/jangomezroberts/PycharmProjects/MLP_Project/"
 remote_path="s1993340@mlp1:mlpractical/"
-local_data_path="/Users/jangomezroberts/MLP_Project/data"
+local_data_path="/Users/jangomezroberts/PycharmProjects/MLP_Project/data"
 remote_data_path="s1993340@mlp1:mlpractical/"
