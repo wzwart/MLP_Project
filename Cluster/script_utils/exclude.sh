2e786cc8cc7a3c81a1af7c881fc3309f6846b145
Andreu650 <60737529+Andreu650@users.noreply.github.com>
2020-03-01 12:08:38 +0000

    Added excluded folders

--- Cluster/script_utils/exclude.sh
+++ Cluster/script_utils/exclude.sh
@@ -1,3 +1,3 @@
 echo "Setting user credentials"
-tx_exclude=( --exclude 'exp_basic/saved_models/' --exclude 'exp_unet/saved_models/' --exclude 'notes/'  --exclude 'script_utils/'   --exclude '.git' --exclude '__pycache__' --exclude '.idea' --exclude '\#015')
+tx_exclude=( --exclude 'exp_basic/saved_models/' --exclude 'exp_unet/saved_models/' --exclude 'notes/'  --exclude 'script_utils/'   --exclude '.git' --exclude '__pycache__' --exclude '.idea' --exclude '\#015' --exclude 'exp_300W_5/' --exclude 'exp_300W_6/' --exclude 'exp_300W_7/' --exclude 'exp_300W_8/' --exclude 'exp_300W_9/' --exclude 'exp_Both/' --exclude 'exp_null/')
 rx_exclude=(--exclude 'data' --exclude '*.py' --exclude '*.json' --exclude '*.sh' --exclude 'exp_basic/saved_models/' --exclude 'exp_unet/saved_models/' --exclude 'notes/'  --exclude 'script_utils/'   --exclude '.git' --exclude '__pycache__' --exclude '.idea' --exclude '\#015')
